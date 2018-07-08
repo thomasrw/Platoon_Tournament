6c3ad200fcce81f8fb2922ef1dd9326fbce1274a
thomasrw <thoma525@email.sc.edu>
2018-07-08 15:41:30 -0400

    adding beginnings of code to parse out all possible combinations

--- tournament.py
+++ tournament.py
@@ -4,6 +4,7 @@
 # import glob
 # import subprocess
 import axelrod as axl
+import itertools
 #requires changes to game.py; zero_determinant.py; memoryone.py
 #working changes to individual strategies
 
@@ -19,9 +20,22 @@ print("hello world")
 
 
 axl.seed(0)  # Set a seed
-# players = [s() for s in axl.demo_strategies]  # Create players
-players = [axl.Cooperator(), axl.Defector(), axl.Random(0.5), axl.TitForTat(), axl.Grudger()]
-tournament = axl.Tournament(players, turns=1)  # Create a tournament
+players = [s() for s in axl.demo_strategies]  # Create players
+#players = [axl.Cooperator(), axl.Defector(), axl.Random(0.5), axl.TitForTat(), axl.Grudger()]
+myplayers = [x for x in itertools.combinations_with_replacement(axl.demo_strategies, 5)]
+
+
+print(players)
+#print(axl.demo_strategies)
+print(myplayers)
+print(len(myplayers))
+
+playthis = [y() for y in myplayers[0]]
+print(playthis)
+
+
+'''
+tournament = axl.Tournament(players)  # Create a tournament
 #tournament.prob_end = 100
 print("turns equals")
 print(tournament.turns)
@@ -29,3 +43,5 @@ results = tournament.play()  # Play the tournament
 print(results.ranked_names)
 
 # ['Defector', 'Grudger', 'Tit For Tat', 'Cooperator', 'Random: 0.5']
+
+'''
