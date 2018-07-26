b8249237d3c62cd020850320133058cae91c8776
thomasrw <thoma525@email.sc.edu>
2018-07-26 05:54:59 -0400

    added call to generate tournament play csv file

--- tournament.py
+++ tournament.py
@@ -5,6 +5,9 @@
 # import subprocess
 import axelrod as axl
 import itertools
+import pickle
+import marshal
+import json
 #requires changes to game.py; zero_determinant.py; memoryone.py
 #working changes to individual strategies not required
 #resolving dependencies required installing some older software versions
@@ -24,12 +27,14 @@ axl.seed(0)  # Set a seed
 players = [s() for s in axl.demo_strategies]  # Create players
 #players = [axl.Cooperator(), axl.Defector(), axl.Random(0.5), axl.TitForTat(), axl.Grudger()]
 #myplayers = [x for x in itertools.combinations_with_replacement(axl.demo_strategies, 10)]
-c = itertools.combinations_with_replacement(axl.demo_strategies, 100)
+c = itertools.combinations_with_replacement(axl.demo_strategies, 50)
 counter = sum( 1 for _ in c)
 print(counter)
+#316251
 
-#TODO use counter with itertools.islice to setup each comibination of players
-myplayers = itertools.combinations_with_replacement(axl.demo_strategies, 100)
+
+
+myplayers = itertools.combinations_with_replacement(axl.demo_strategies, 50)
 
 print(players)
 #print(axl.demo_strategies)
@@ -38,12 +43,18 @@ print(players)
 
 #playthis = [y() for y in myplayers[0]]
 
-for z in range(1):
+file_prefix = "/home/thomasrw/Desktop/"
+#f = open(file_prefix + 'run1', 'w')
+
+
+for z in range(2):
     playthis = [y() for y in next(myplayers)]
-    print(playthis)
-    tournament = axl.Tournament(playthis)
-    results = tournament.play()
+    #print(playthis)
+    tournament = axl.Tournament(playthis, turns=10)
+    results = tournament.play(filename=file_prefix + 'run' + str(z+1))
     print(results.ranked_names)
+
+#TODO reconstitute result_set object from file generated previously by tournament.play()
 
 
 
