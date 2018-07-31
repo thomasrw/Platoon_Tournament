bda23ed1633acba4b50ddf449fc3b36353f20fa3
thomasrw <thoma525@email.sc.edu>
2018-07-31 06:14:49 -0400

    updated notes

---
+++ consolidated_results.py
@@ -0,0 +1,9 @@
+# @thomasrw
+# copyright 2018
+
+
+#Methods to read in multiple tournament result_sets and analyze results
+
+#TODO recover results
+#TODO determine key data to capture
+#TODO iterate over results capturing key data
