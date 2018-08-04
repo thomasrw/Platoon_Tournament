90f76d66d7685fdf34b770f55a6996ebcf3b562c
thomasrw <thoma525@email.sc.edu>
2018-08-04 11:03:19 -0400

    add get_avg_score add update_max_scores

--- consolidated_results.py
+++ consolidated_results.py
@@ -29,12 +29,81 @@ def recover_results(p,r):
 #Total victor overall
 #Max_system score (%) compared to Pareto Optimal, ie a platoon always forms
 
-#TODO investigate matches vs tourney and how play(self) is counted
+#TODO investigate matches vs tourney and how play(self) is counted? - or account for it in results analysis
 #TODO iterate over results capturing key data
+
+
+def get_avg_score(results, player_num):
+    c = sum(z for z in results.scores[player_num]) / results.repetitions
+    return c
+
+strategies = [s() for s in axl.demo_strategies]
+#[Cooperator, Defector, Tit For Tat, Grudger, Random: 0.5]
+Max_Strategy_Scores = [0,0,0,0,0]
+Best_Runs = [-1, -1, -1, -1, -1]
+
+#Max_Defector_Score = 0
+#Max_Grudger_Score = 0
+#Max_TitForTat_Score = 0
+Max_Cooperator_Score = 0
+#Max_Random_Score = 0
+
+
+def update_max_scores(results):
+    global Max_Strategy_Scores
+    global strategies
+    for z in range(results.players.__len__()):
+        #print(results.players[z])
+        for y in range(len(strategies)):
+            if str(results.players[z]) == str(strategies[y]):
+                c = get_avg_score(results, z)
+                if c > Max_Strategy_Scores[y]:
+                    Max_Strategy_Scores[y] = c
+                    # TODO reset best run list and add run that the max score was achieved in
+                elif c == Max_Strategy_Scores[y]:
+                    print("test")
+
+                    #TODO append run to list of runs that max score was achieved in
+
+
+
+def update_max(results):
+    global Max_Cooperator_Score
+    for z in range(results.players.__len__()):
+        print(results.players[z])
+        if str(results.players[z]) == 'Cooperator':
+            c = get_avg_score(results, z)
+            if c > Max_Cooperator_Score:
+                Max_Cooperator_Score = c
+
 
 
 print("Goliath online")
 
-results = recover_results(file_path,1000)
+results = recover_results(file_path,2)
 print(results.ranked_names)
+print(results.players)
+print(results.players[0])
+print(results.scores)
+print(results.scores[49])
 
+
+print( get_avg_score(results, 0))
+print(results.players.__len__())
+
+print(results.repetitions)
+
+print(Max_Cooperator_Score)
+update_max(results)
+print(Max_Cooperator_Score)
+
+print(strategies)
+update_max_scores(results)
+print(Max_Strategy_Scores)
+update_max_scores(results)
+
+for x in range(10):
+    update_max_scores(recover_results(file_path,x+1))
+print(strategies)
+print(Max_Strategy_Scores)
+print(Best_Runs)
